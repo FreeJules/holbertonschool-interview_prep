8ba18eea5190fe04f0ff6d279e1b1832941ecd3f
Julija Lee <lee.julija@gmail.com>
2018-05-20 12:41:17 +0000

    changed it to faster recursive version

--- 0x02-linked_list_palindrome/0-is_palindrome.c
+++ 0x02-linked_list_palindrome/0-is_palindrome.c
@@ -3,71 +3,27 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list
- * @head: pointer to first node in the list
- * @n: value of second node in the list
- * Return: the address of the new element, or NULL if it failed
- */
-listint_t *add_node(listint_t **head, int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (*head);
-}
-
-/**
- * reverse - makes a reversed copy of a list
- * @head_ref: pointer to first node of a reversed list
- * @head: pointer to first node of original list
- * Return: None
- */
-void reverse(listint_t **head_ref, listint_t **head)
-{
-	listint_t *tmp = *head;
-
-	while (tmp)
-	{
-		add_node(head_ref, tmp->n);
-		tmp = tmp->next;
-	}
-}
-
-/**
- * compare_lists - compares original and reversed list
- * @head1: first node of one list
- * @head2: first node of other list
+ * check_ends - checks if ends the same through recursion backwards
+ * @head: pointer to first node of one list
+ * @ret: pointer to last node of the list
  * Return: 1 if they are same, 0 if they are not
  */
-int compare_lists(listint_t *head1, listint_t *head2)
+int check_ends(listint_t *head, listint_t **ret)
 {
-	listint_t *temp1 = head1;
-	listint_t *temp2 = head2;
+	int tmp = 0;
 
-	while (temp1 && temp2)
+	if (head->next == NULL)
 	{
-		if (temp1->n == temp2->n)
-		{
-			temp1 = temp1->next;
-			temp2 = temp2->next;
-		}
-		else
-			return (0);
+		tmp = 1;
 	}
-
-	/* both are empty so they are the same*/
-	if (temp1 == NULL && temp2 == NULL)
-		return (1);
-
-	/* one list is not NULL, so they are different */
-	return (0);
+	else
+	{
+		tmp = check_ends(head->next, ret);
+	}
+	tmp = tmp & (head->n == (*ret)->n);
+	*ret = (*ret)->next;
+	return (tmp);
 }
-
 /**
  * is_palindrome - checks if linked list is palindrome
  * @head: pointer to first node of one list
@@ -75,12 +31,8 @@ int compare_lists(listint_t *head1, listint_t *head2)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head1 = NULL;
-	int ret;
-
-	if (!(*head) || !((*head)->next))
+	if (!head)
 		return (1);
-	reverse(&head1, head);
-	ret = compare_lists(*head, head1);
-	return (ret);
+
+	return (check_ends(*head, head));
 }
