649d4ab681a4936bb126b01f0807776740356d99
Julija Lee <lee.julija@gmail.com>
2018-06-10 23:23:23 +0000

    fixed comments and return statement

--- 0x03-palindrome_integer/0-is_palindrome.c
+++ 0x03-palindrome_integer/0-is_palindrome.c
@@ -2,16 +2,20 @@
 /**
  * is_palindrome - checks whether or not a given integer is a palindrome
  * @n: the number to be checke
+ *
+ * Return: 1 if n is a palindrome, and 0 otherwise
  */
 int is_palindrome(unsigned long n)
 {
 	unsigned long tmp = n;
 	unsigned long reversed_n = 0;
 
-	while (tmp != 0)
+	while (tmp > 0)
 	{
 		reversed_n = reversed_n * 10 + tmp % 10;
 		tmp = tmp / 10;
 	}
-	return (n == reversed_n);
+	if (n == reversed_n)
+		return (1);
+	return (0);
 }
